8d0ccdc741d5baf890541f3def3e2e64df32d84a
Huynh Chi Bao <baohc110902@gmail.com>
2022-01-10 22:18:00 +0700

    importStaffs-from-shared

--- run/src/App.js
+++ run/src/App.js
@@ -1,5 +1,7 @@
 import React, { Component } from 'react';
 import StaffList from './components/StaffListcomponent';
+import { STAFFS } from './shared/staffs';
+
 class App extends Component {
   render() {
     return (
