a0e988c9d4ed3bd6a8f29fffc798b51f724902b7
Huynh Chi Bao <baohc110902@gmail.com>
2022-01-10 22:42:43 +0700

    set-OnclickStaff

--- run/src/components/StaffListcomponent.js
+++ run/src/components/StaffListcomponent.js
@@ -1,19 +1,37 @@
 import React, { Component } from 'react';
 
 class StaffList extends Component {
+
+    constructor(props) {
+        super(props);
+        this.state = {
+            SelectedStaff: null
+        }
+    }
+
+
+    onSelectedStaff(staff){
+        this.setState({SelectedStaff: staff})
+    }
+
     render() {
         const menu = this.props.staffs.map((staff) => {
             return (
-                    <div className='col-12 col-md-6 col-lg-4'>
-                        <ul key={staff.id}>
-                            <li>{staff.name}</li>
-                        </ul>
-                    </div>
+                <div className='col-12 col-md-6 col-lg-4'>
+                    <ul key={staff.id} onClick={() => this.onSelectedStaff(staff)}>
+                        <li>{staff.name}</li>
+                    </ul>
+                </div>
             )
         });
         return (
-            <div className='row'>
-                {menu}
+            <div>
+                <div className='row'>
+                    {menu}
+                </div>
+                <div className='row'>
+                    
+                </div>
             </div>
         );
     }
