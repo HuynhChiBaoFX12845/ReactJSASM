fd60a77f569409461d653ac1f07a016224d34e20
Huynh Chi Bao <baohc110902@gmail.com>
2022-01-10 22:15:25 +0700

    importStaffListcomponent-to-Appjs

--- run/src/App.js
+++ run/src/App.js
@@ -1,10 +1,10 @@
 import React, { Component } from 'react';
-
+import StaffList from './components/StaffListcomponent';
 class App extends Component {
   render() {
     return (
       <div>
-        
+        <StaffList/>
       </div>
     );
   }
