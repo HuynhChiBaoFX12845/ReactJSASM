9cb753b777ae18fa25d2aaf3766fb4d3686d3728
Huynh Chi Bao <baohc110902@gmail.com>
2022-01-20 22:24:23 +0700

    import-PresentationComponents-To-ContainerComponents

--- run/src/components/MainComponent.js
+++ run/src/components/MainComponent.js
@@ -1,4 +1,11 @@
 import React, { Component } from 'react';
+import MenuStaff from './MenuStaffComponent';
+import DepartmentStaff from './DepartmentStaff';
+import Footer from './FooterComponent';
+import Header from './HeaderComponent';
+import StaffList from './StaffListcomponent';
+import SalaryStaff from './SalaryStaff';
+import { STAFFS } from '../shared/staffs'
 
 class Main extends Component {
     render() {
